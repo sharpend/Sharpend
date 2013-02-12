e7dca8bd0c3e211c96eb661c2d7c53df6e64d4be
dirk <sharpend_develop@yahoo.de>
2013-02-12 11:07:02 +0700

    split into a sharpend.dll and sharpend.gtk.dll

---
+++ Sharpend.Gtk/Main.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Sharpend.Gtk
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
---
+++ Sharpend.Gtk/Utils/Extensions.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Sharpend.Gtk
+{
+	public class Extensions
+	{
+		public Extensions ()
+		{
+		}
+	}
+}
+
